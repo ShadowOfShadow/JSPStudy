d7c2ae4c952e885c4325ba96a076dd1d565e66a6
limcw <limcw@DESKTOP-RTF007B>
2021-06-27 18:15:43 +0900

    31강까지

--- src/main/java/com/newlecture/web/Calc2.java
+++ src/main/java/com/newlecture/web/Calc2.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,8 +16,9 @@ public class Calc2 extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		//ServletContext application = request.getServletContext(); //상태값 저장하기 위한 서블릿컨텍스트
+		ServletContext application = request.getServletContext(); //상태값 저장하기 위한 서블릿컨텍스트
 		HttpSession session = request.getSession(); // 세션
+		Cookie[] cookies = request.getCookies(); // 쿠키값들 받기
 		
 		response.setCharacterEncoding("UTF-8"); // 서버가 UTF-8 형식으로 보낸다
 		response.setContentType("text/html; charset=UTF-8"); // 클라이언트에게 UTF-8 형식으로 읽으라고 명시
@@ -31,10 +33,28 @@ public class Calc2 extends HttpServlet{
 		if(op.equals("=")) {
 			
 			//int x = (Integer)application.getAttribute("value");
-			int x = (Integer)session.getAttribute("value");
+			//int x = (Integer)session.getAttribute("value");
+			int x = 0;
+			
+			// 클라이언트에게 받은 쿠키 검색
+			for(Cookie c : cookies) 
+				if(c.getName().equals("value")) {
+					x = Integer.parseInt(c.getValue());
+					break;
+				}
+			
+				
 			int y = v;
 			//String operator =(String)application.getAttribute("op");
-			String operator =(String)session.getAttribute("op");
+			//String operator =(String)session.getAttribute("op");
+			
+			String operator ="";
+			for(Cookie c : cookies) 
+				if(c.getName().equals("op")) {
+					operator =c.getValue();
+					break;
+				}
+			
 			int result = 0;
 			
 			if(operator.equals("+"))
@@ -48,9 +68,18 @@ public class Calc2 extends HttpServlet{
 		else {
 //			application.setAttribute("value", v);
 //			application.setAttribute("op", op);
-			session.setAttribute("value", v);
-			session.setAttribute("op", op);
-		}		
+			//session.setAttribute("value", v);
+			//session.setAttribute("op", op);
+			
+			// 클라이언트에게 쿠키값 생성해 보내기
+			Cookie valueCookie = new Cookie("value", String.valueOf(v));
+			Cookie opCookie = new Cookie("op", op);
+			valueCookie.setPath("/calc2");
+			opCookie.setPath("/calc2");
+			response.addCookie(valueCookie); 
+			response.addCookie(opCookie);
+		}
+		
 		
 	}
 
