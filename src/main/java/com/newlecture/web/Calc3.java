94a36830f99246e3f65fa2572df9d3423fa817dd
limcw <limcw@DESKTOP-RTF007B>
2021-06-28 19:16:33 +0900

    동적페이지(계산기)

---
+++ src/main/java/com/newlecture/web/Calc3.java
@@ -0,0 +1,66 @@
+package com.newlecture.web;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/calc3")
+public class Calc3 extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		Cookie[] cookies = request.getCookies(); // 쿠키값들 받기		
+				
+		String value = request.getParameter("value");
+		String operator = request.getParameter("operator");
+		String dot = request.getParameter("dot");
+		
+		String exp = ""; 
+		//쿠키 가져오기
+		if(cookies !=null)
+			for(Cookie c : cookies) 
+				if(c.getName().equals("exp")) {
+					exp = c.getValue();
+					break;
+				}
+		
+		//계산
+		if(operator != null && operator.equals("=")) {
+			ScriptEngineManager manager = new ScriptEngineManager();
+			ScriptEngine engine = manager.getEngineByName("JavaScript");
+			try {
+				exp = String.valueOf(engine.eval(exp));
+			} catch (ScriptException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}else { //누적
+		
+			exp += (value == null) ?"":value;
+			exp += (operator == null) ?"":operator;
+			exp += (dot == null) ?"":dot;
+		}		
+		
+		Cookie expCookie = new Cookie("exp", exp);		
+		response.addCookie(expCookie);
+		response.sendRedirect("calcpage");
+		
+		
+		
+		
+	}
+
+}
